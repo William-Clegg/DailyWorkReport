91b8472032747f42cfa85cbbc947755cf3d079c0
William-Clegg <wf.clegg@gmail.com>
2018-08-07 21:32:52 -0400

    first kotlin

---
+++ app/src/main/java/stasco_mech/com/stasco/TimeFormDetailFragment.kt
@@ -0,0 +1,9 @@
+package stasco_mech.com.stasco
+
+import android.support.v4.app.Fragment
+import android.widget.ImageButton
+
+class TimeFormDetailFragment : Fragment() {
+    var addArea: ImageButton
+
+}
